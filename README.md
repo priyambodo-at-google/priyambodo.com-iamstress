7ecbff8aab76173058eaf12a089023d4efe2baca
Doddi Priyambodo <doddi@bicarait.com>
2024-06-06 23:23:39 +0700

    Doddi Priyambodo is committing at 2024-06-06 23:23:39

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# priyambodo.com-iamstress
+#Highly Scalable and Powerful Stress Tester tool to Hit the Performance of for Your Application (DDOS?)!
+## priyambodo.com-iamstress
+
+https://bicarait.com/blog/2024/06/05/application-performance-test-for-your-app/
+
--- github.sh
+++ github.sh
@@ -5,16 +5,16 @@ git add .
 git commit -a -m "Doddi Priyambodo is committing at $(date +"%Y-%m-%d %H:%M:%S")"
 git push
 
-#'----------------------------------'
-echo "# priyambodo.com-iamstress" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/priyambodo-at-google/priyambodo.com-iamstress.git
-git push -u origin main
+# #'----------------------------------'
+# echo "# priyambodo.com-iamstress" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/priyambodo-at-google/priyambodo.com-iamstress.git
+# git push -u origin main
 
-#'----------------------------------'
-git remote add origin https://github.com/priyambodo-at-google/priyambodo.com-iamstress.git
-git branch -M main
-git push -u origin main
+# #'----------------------------------'
+# git remote add origin https://github.com/priyambodo-at-google/priyambodo.com-iamstress.git
+# git branch -M main
+# git push -u origin main
